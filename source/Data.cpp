82859bc3e4c4ee17f4bd77b9472ff5a99c77b452
LEROY Remy <remy.leroy@insa-rennes.fr>
2026-01-13 16:36:58 +0100

    ajout com

--- source/Data.cpp
+++ source/Data.cpp
@@ -2,33 +2,32 @@
 
 void Data::load(const string& file_name) {
     std::ifstream file(file_name);
+
     if (file) {
+
+        string line;                // Ligne en cours de lecture
+        vector<double> tabFeatures; // Tableaux de features
+        double feature;             // Valeur d'une feature
+        uint8_t tag;                // Note associée au sample
+
         file >> _nbSamples;     // Recuperation premiere ligne du fichier : nombre de 'samples'
         file >> _nbFeatures;    // Recuperation deuxieme ligne du fichier : nombre de 'features' par 'samples'
-
-        cout << "Samples : " << _nbSamples << " and Features : " << _nbFeatures << endl;
-
-        string line;
-        vector<double> tabFeatures; // Tableaux de features
-        double feature;
-        uint8_t tag;                // Notation attribue au sujet etudie
-
-        file.ignore(1, ' ');
+        file.ignore(1, ' ');    // Ignorer le saut de ligne après le nombre de features
+        //cout << "Samples : " << _nbSamples << " and Features : " << _nbFeatures << endl;
         
         for(int i = 0 ; i < _nbSamples ; i++) {
-            std::getline(file, line);       // lecture d'une ligne complete (debut troisieme ligne)
-            //cout << "\nLigne " << i << " : " << line << endl;
-
-            std::istringstream iss(line);   // flux de la ligne en cours
-            iss >> tag;                     // recuperation du tag
-            cout << "Note donner : " << tag << endl;
-
-            tabFeatures.clear();            // Vide le tableau (nouveau sample)
+            tabFeatures.clear();            // Réinitialisation du tableau de features
+            std::getline(file, line);       // Lecture d'une ligne complète
+            std::istringstream iss(line);   // Flux pour extraire les données de la ligne
+            iss >> tag;                     // Extraction de la note (tag)
+            //cout << "\nLigne " << i << " : " << line << endl;            
+            //cout << "Note donner : " << tag << endl;
 
             while(iss >> feature) {
-                tabFeatures.push_back(feature);
+                tabFeatures.push_back(feature); // Extraction des features et ajout au tableau
+                //cout << "Feature : " << feature << endl;
             }
-            //Sample(tag, tabFeatures);
+            Sample(tag, tabFeatures);
         }
         file.close();
         return;
